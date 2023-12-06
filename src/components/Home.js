792b12713ede7b52fe2941df7eadfb923381cd2b
shivendra <shivendrakeshari11@gmail.com>
2023-12-06 14:00:55 +0530

    made changes_1

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <div>
       <ToastContainer/>
-      <h1 className='mx-5 my-5'>Spring-Boot Application using React web/app   </h1>
+      <h1 className='mx-5 my-5'>Spring-Boot Application using React web/app  </h1>
       <Button color='primary text-center mx-5' size='lg' outline>Create a toast    </Button>
       <Course course={{title: "Android developement Course", description: "This course contains tutorials about all the basic implementations of android app."}}/>
     </div>
